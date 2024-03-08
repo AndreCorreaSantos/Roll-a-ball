2f85f5766e243ca11ca38f7be1d821a9180b9b7e
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-03-08 17:31:56 -0300

    adding working roll-a-ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,9 +13,14 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
+    public TextMeshProUGUI countText;
+
+    private int count;
     void Start()
     {
-        rb = GetComponent <Rigidbody>();    
+        rb = GetComponent <Rigidbody>();
+        count = 0;    
+        SetCountText();
     }
 
    void OnMove (InputValue movementValue)
@@ -32,11 +39,17 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other) 
     {
-        Debug.Log(other.gameObject.tag);
         if (other.gameObject.CompareTag("PickUp")) 
                 {
                     other.gameObject.SetActive(false);
+                    count += 1;
+                    SetCountText();
                 }
     }
+
+   void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+   }
 
 }
